2fb63629c805e414394ea1e5eae34905aab4e210
Nicolas Eckell <niceckell@gmail.com>
2022-03-01 04:27:23 -0300

    Change to mongoDB

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,97 +1,64 @@
 var express = require("express");
-const neo4j = require("neo4j-driver");
 var router = express.Router();
 
-const driver = neo4j.driver(
-	"bolt://3.92.251.249:7687",
-	neo4j.auth.basic("neo4j", "gun-fantail-hydraulics"),
-	{
-		/* encrypted: 'ENCRYPTION_OFF' */
+const { MongoClient } = require("mongodb");
+const uri =
+	"mongodb+srv://card-managment-test:jR5CYm48XodiRknF@cluster0.pqwkz.mongodb.net/cm_test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function main() {
+	try {
+		// Connect to the MongoDB cluster
+		await client.connect();
+
+		// Make the appropriate DB calls
+		await listDatabases(client);
+	} catch (e) {
+		console.error(e);
+	} finally {
+		// Close the connection to the MongoDB cluster
+		await client.close();
 	}
-);
+}
+
+main().catch(console.error);
+
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
+
+	console.log("Databases:");
+	databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
 
 /* GET purchases listing. */
-router.get("/", function (req, res, next) {
-	const query = `
-	MATCH (movie:Movie {title:$favorite})<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
-	RETURN distinct rec.title as title LIMIT 3
-	`;
-
-	const params = { favorite: "The Matrix" };
-
-	const session = driver.session({ database: "neo4j" });
-
-	session
-		.run(query, params)
-		.then((result) => {
-			result.records.forEach((record) => {
-				console.log(record.get("title"));
-			});
-			session.close();
-			driver.close();
-			res.send(result.records);
-		})
-		.catch((error) => {
-			console.error(error);
-			res.send([]);
-		});
-
-	// res.send([
-	// 	{
-	// 		id: "1",
-	// 		state: "Activo",
-	// 		store_name: "ArmyTech",
-	// 		purchasing_date: "nov/20",
-	// 		item_name: "Placa",
-	// 		total_amount: 47250,
-	// 		bank: "Santander",
-	// 		card: "Visa",
-	// 	},
-	// 	{
-	// 		id: "2",
-	// 		state: "Activo",
-	// 		store_name: "ArmyTech2",
-	// 		purchasing_date: "dic/20",
-	// 		item_name: "Placa2",
-	// 		total_amount: 60000,
-	// 		bank: "Santander",
-	// 		card: "Visa",
-	// 	},
-	// ]);
-});
+router.get("/", function (req, res, next) {});
 
 router.post("/", function (req, res, next) {
-	console.log(req.body);
+	// console.log(req.body);
 	let errors = validateInput(req.body);
 	if (errors.length !== 0) {
 		res.status(400);
 		res.send(errors);
 	}
+
+	// const main = async () => {
+	client.connect((err) => {
+		console.log("asdas");
+		const collection = client.db("cm_test").collection("QuotaPurchase");
+		collection.insertOne(req.body).then(() => {
+			client.close();
+		});
+	});
+	// };
+	// main();
+
 	res.status(201);
 	res.send("ok");
-	// const query = `
-	// MATCH (movie:Movie {title:$favorite})<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
-	// RETURN distinct rec.title as title LIMIT 3
-	// `;
-
-	// const params = { favorite: "The Matrix" };
-
-	// const session = driver.session({ database: "neo4j" });
-
-	// session
-	// 	.run(query, params)
-	// 	.then((result) => {
-	// 		result.records.forEach((record) => {
-	// 			console.log(record.get("title"));
-	// 		});
-	// 		session.close();
-	// 		driver.close();
-	// 		res.send(result.records);
-	// 	})
-	// 	.catch((error) => {
-	// 		console.error(error);
-	// 		res.send([]);
-	// 	});
 });
 
 const validateInput = (data) => {
