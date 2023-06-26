8f8be8c951e2770668237dcf69c25ea812ffb09d
Nicolas Eckell <niceckell@gmail.com>
2023-06-26 10:50:51 -0300

    dotenv + mongo db

--- app/DAL/MongoDriver.js
+++ app/DAL/MongoDriver.js
@@ -2,7 +2,9 @@ const { MongoClient } = require("mongodb");
 
 let _client = null;
 const uri =
-	"mongodb+srv://card-managment-test:jR5CYm48XodiRknF@cluster0.pqwkz.mongodb.net/cm_test?retryWrites=true&w=majority";
+	process.env.MONGO_URL +
+	process.env.MONGO_DB +
+	"?retryWrites=true&w=majority";
 
 const connectToClientDB = async function () {
 	if (_client !== null) return _client;
